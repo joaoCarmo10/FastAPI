0bcb524c6a42a7146b2cdd98a494fa0fac4c1641
Carmo <joaocarmocs@gmail.com>
2023-11-26 15:20:29 -0300

    Novas inserções

--- Code/consulta_curso.py
+++ Code/consulta_curso.py
@@ -1,6 +1,6 @@
-from fastAPI import fastAPI
+from fastapi import FastAPI, HTTPException
 
-app = fastAPI()
+app = FastAPI()
 
 cursos = {
     1:{
@@ -11,12 +11,33 @@ cursos = {
     2:{
         "nome": "Automação de tarefas",
         "nivel": "intermediário",
-        "formacao": "Automação"
+        "formacao": "Automacao"
     },
     3:{
         "nome": "Automação com Selenium",
         "nivel": "intermediário",
-        "formacao": "Automação"
+        "formacao": "Automacao"
     }
 }
 
+@app.get('/cursos/{formacao}')
+async def get_cursos_formacao(formacao:str):
+    cursos_formacao = [
+        curso for curso in cursos.values()
+        if curso['formacao'].lower() == formacao.lower()
+    ]
+    return cursos_formacao
+
+
+@app.get('/cursos/')
+async def get_cursos_formacao_query(formacao:str):
+    cursos_formacao = [
+        curso for curso in cursos.values()
+        if curso['formacao'].lower() == formacao.lower()
+    ]
+    if not cursos_formacao:
+        raise HTTPException(
+            status_code=404,
+            detail='Formação não encontrada'
+        )
+    return cursos_formacao
